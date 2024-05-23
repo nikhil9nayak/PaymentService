e0f7c50dd1cdffde57a8f9f997e2f01e72790527
NIKHIL <nikhil9nayak@gmail.com>
2024-05-23 18:17:52 +0530

    Day-225: A Webhooks added for Stripe PG

---
+++ src/main/java/com/paymentservice/paymentservice/controllers/StripeWebhookController.java
@@ -0,0 +1,16 @@
+package com.paymentservice.paymentservice.controllers;
+
+import com.stripe.model.Event;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stripeWebhook")
+public class StripeWebhookController {
+
+    @PostMapping
+    public void stripeWebhook(Event event){   // After a Event successfully completed, this webhook will auto trigger. Event can be anything like Payment Successful, Payment Failed, Payment Pending etc
+        System.out.println("Stripe Webhooks");
+    }
+}
